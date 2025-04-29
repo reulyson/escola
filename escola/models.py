287efd956ea7cd04a86d424ab6160eaa6f263361
Reulyson Wanzeler <reulyson@gmail.com>
2025-04-29 17:00:44 -0400

    feat: Criando validações no Models

--- escola/models.py
+++ escola/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 ''' Modelo com os campos da tabela estudante '''
 class Estudante(models.Model):
     nome = models.CharField(max_length=100)
     email = models.EmailField(blank=False, max_length=30)
-    cpf = models.CharField(max_length=11)
+    cpf = models.CharField(max_length=11, unique=True)
     data_nascimento = models.DateField()
     celular = models.CharField(max_length=14)
 
@@ -22,7 +23,8 @@ class Curso(models.Model):
         ('A', 'Avançado'),
     )
 
-    codigo = models.CharField(max_length=10)
+    # validando o valor de código como valor único e com 3 dígitos no minimo
+    codigo = models.CharField(max_length=10, unique=True, validators=[MinLengthValidator(3)])
     descricao = models.CharField(max_length=100, blank=False)
     nivel = models.CharField(max_length=1, choices=NIVEL, blank=False, null=False, default='B')
 
